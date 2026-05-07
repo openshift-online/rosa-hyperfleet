rosa-regional-platform-ci/rosa-regional-platform

adds CI prefix for use in PD Service Name

scripts/buildspec/provision-infra-rc.sh
@@ -110,6 +110,7 @@ fi
 # Extract regional_id and environment from rendered config
 export TF_VAR_regional_id=$(jq -r '.regional_id' "$DEPLOY_CONFIG_FILE")
 export TF_VAR_environment=$(jq -r '.environment' "$DEPLOY_CONFIG_FILE")
+export TF_VAR_ci_prefix=$(jq -r '.ci_prefix // ""' "$DEPLOY_CONFIG_FILE")
 
 echo "Terraform variables:"
 echo "  Region: $TF_VAR_region"

terraform/modules/pagerduty-service/main.tf
@@ -7,7 +7,7 @@
 # =============================================================================
 
 locals {
-  service_name = "rrp-${var.environment}-${var.region}"
+  service_name = var.ci_prefix != "" ? "rrp-${var.ci_prefix}-${var.environment}-${var.region}" : "rrp-${var.environment}-${var.region}"
 }
 
 # =============================================================================

openshift-online/rosa-hyperfleet

scripts/buildspec/provision-infra-rc.sh
@@ -112,6 +112,7 @@ fi
 # Extract regional_id and environment from rendered config
 export TF_VAR_regional_id=$(jq -r '.regional_id' "$DEPLOY_CONFIG_FILE")
 export TF_VAR_environment=$(jq -r '.environment' "$DEPLOY_CONFIG_FILE")
+export TF_VAR_eph_prefix=$(jq -r '.eph_prefix // ""' "$DEPLOY_CONFIG_FILE")
 
 echo "Terraform variables:"
 echo "  Region: $TF_VAR_region"

terraform/modules/pagerduty-service/main.tf
@@ -7,7 +7,7 @@
 # =============================================================================
 
 locals {
-  service_name = "rrp-${var.environment}-${var.region}"
+  service_name = var.eph_prefix != "" ? "rrp-${var.eph_prefix}-${var.environment}-${var.region}" : "rrp-${var.environment}-${var.region}"
 }
 
 # =============================================================================